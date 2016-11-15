6bd1a10b3e6dc6012998a159f5b7cc3dbeddbd28
cheng10 <cheng10@ualberta.ca>
2016-11-14 17:06:00 -0700

    add few tests

--- mords_backend/mords/tests.py
+++ mords_backend/mords/tests.py
@@ -1,3 +1,33 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from mords_api.models import Note
 
-# Create your tests here.
+
+class NoteMethodTests(TestCase):
+    def test_was_published_recently_with_future_note(self):
+        """
+        was_published_recently() should return False for notes whose
+        pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_note = Note(pub_date=time)
+        self.assertIs(future_note.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for notes whose
+        pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_note = Note(pub_date=time)
+        self.assertIs(old_note.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for notes whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_note = Note(pub_date=time)
+        self.assertIs(recent_note.was_published_recently(), True)
--- mords_backend/mords_api/models.py
+++ mords_backend/mords_api/models.py
@@ -57,8 +57,8 @@ class Note(models.Model):
         ordering = ['word', 'text', 'author']
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
