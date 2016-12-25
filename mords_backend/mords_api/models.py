70274c799b6659f98fa7f5e76e9b4d84bd418c5d
TeppieC <teppie.c@hotmail.com>
2016-12-25 02:27:52 -0700

    update

--- mords_backend/mords_api/models.py
+++ mords_backend/mords_api/models.py
@@ -20,7 +20,7 @@ class Word(models.Model):
         return self.text
 
     class Meta:
-        ordering = ['text']
+        ordering = ['-update_date']
 
 
 @python_2_unicode_compatible
@@ -49,7 +49,7 @@ class Entry(models.Model):
         return self.word.text
 
     class Meta:
-        ordering = ['word']
+        ordering = ['-update_date']
 
 
 @python_2_unicode_compatible
